0383d3c2d502a848b3ed61e1bd93286119647394
Zaenal Abidin <zaenalabmath@gmail.com>
2024-07-02 22:55:58 +0800

    mengatur data base

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'db_ppdb');
+$conn = mysqli_connect('localhost', 'rou966478672_ppdb', 'Anaksholeh!7', 'u966478672_ppdb');
+// $conn = mysqli_connect('localhost', 'root', '', 'db_ppdb');
 
 //check connetion
 // if ($conn) {
